3298114d9c8f850dac37485eb9e869b0d1eff84f
kojo frempong <kojof@hotmail.com>
2021-01-03 17:47:35 +0000

    Add project files.

---
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ConstructionLine.CodingChallenge.Tests
+{
+    [TestFixture]
+    public class SearchEngineTests : SearchEngineTestsBase
+    {
+        [Test]
+        public void Test()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> {Color.Red},
+                Sizes = new List<Size> {Size.Small}
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            AssertResults(results.Shirts, searchOptions);
+            AssertSizeCounts(shirts, searchOptions, results.SizeCounts);
+            AssertColorCounts(shirts, searchOptions, results.ColorCounts);
+        }
+
+
+        [Test]
+        public void When_Null_SearchOptions_Expect_Null_SearchResults()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+
+            var results = searchEngine.Search(null);
+
+            Assert.AreEqual(null, results.ColorCounts);
+            Assert.AreEqual(null, results.Shirts);
+            Assert.AreEqual(null, results.SizeCounts);
+        }
+
+
+        [Test]
+        public void When_Empty_SearchOptions_Expect_Null_SearchResults()
+        {
+            var shirts = new List<Shirt>
+            {
+                new Shirt(Guid.NewGuid(), "Red - Small", Size.Small, Color.Red),
+                new Shirt(Guid.NewGuid(), "Black - Medium", Size.Medium, Color.Black),
+                new Shirt(Guid.NewGuid(), "Blue - Large", Size.Large, Color.Blue),
+            };
+
+            var searchEngine = new SearchEngine(shirts);
+
+            var searchOptions = new SearchOptions
+            {
+                Colors = new List<Color> { },
+                Sizes = new List<Size> {  }
+            };
+
+            var results = searchEngine.Search(searchOptions);
+
+            Assert.AreEqual(null, results.ColorCounts);
+            Assert.AreEqual(null, results.Shirts);
+            Assert.AreEqual(null, results.SizeCounts);
+
+        }
+
+
+        [Test]
+        public void When_Null_Shirts_Throw_NullArgumentException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SearchEngine(null));
+        }
+    }
+}
---
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Xml.Schema;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class SearchEngine
+    {
+        private readonly List<Shirt> _shirts;
+
+        public SearchEngine(List<Shirt> shirts)
+        {
+            _shirts = shirts ?? throw new ArgumentNullException(nameof(shirts));
+
+            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
+
+        }
+
+
+        public SearchResults Search(SearchOptions options)
+        {
+            //  var shirts = _shirts.AsEnumerable();
+
+            //var colorCounts = from shirt in shirts
+            //    where options.Colors.Contains(shirt.Color) 
+            //    group shirt by shirt.Color
+            //    into colorGroup
+            //    select new ColorCount()
+            //    {
+            //        Color = colorGroup.Key,
+            //        Count = colorGroup.Count()
+            //    };
+
+            //var sizeCounts = from shirt in shirts
+            //    where options.Sizes.Contains(shirt.Size)
+            //    group shirt by shirt.Size
+            //    into sizeGroup
+            //    select new SizeCount()
+            //    {
+            //        Size = sizeGroup.Key,
+            //        Count = sizeGroup.Count()
+            //    };
+           
+
+            // TODO: search logic goes here.
+
+            if (options != null)
+            {
+                if (options.Sizes.Any() || options.Colors.Any())
+                {
+                    var shirts = _shirts.Where(shirt => options.Colors.Contains(shirt.Color) || options.Sizes.Contains(shirt.Size)).ToList();
+
+                    return new SearchResults
+                    {
+                        Shirts = shirts,
+
+                        SizeCounts = Size.All.Select(size => new SizeCount
+                        {
+                            Size = size,
+                            Count = shirts.Count<Shirt>(shirt => shirt.Size == size)
+                        }).ToList(),
+
+                        ColorCounts = Color.All.Select(color => new ColorCount
+                        {
+                            Color = color,
+                            Count = shirts.Count<Shirt>(shirt => shirt.Color == color)
+                        }).ToList()
+                    };
+                }
+            }
+            return new SearchResults();
+        }
+    }
+}
